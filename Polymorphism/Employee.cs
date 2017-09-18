3f3c5da632529211a8f7a1b8569129178d89c4be
Bogdan Mitrache <devmngr@users.noreply.github.com>
2017-09-19 00:50:48 +0200

    Polymorphism done (EX3)

---
+++ Polymorphism/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+        public abstract class Employee
+        {
+            public string name { get; }
+
+            public Employee(string name)
+            {
+                this.name = name;
+            }
+    
+            public abstract double getMonthSalary();
+
+        }
+    
+}
